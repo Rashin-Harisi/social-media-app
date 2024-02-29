dc067726bbb4db461d30760eea5505f3f411be08
Rashin Harisi <rashin.harisi@gmail.com>
2024-02-29 14:54:21 +0100

    creating sign-in page

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -5,4 +5,9 @@ export const SignUpValidation = z.object({
     username: z.string().min(2,{message: 'Too Short'}),
     email : z.string().email(),
     password : z.string().min(8,{message:'Password must be at least 8 characters'}),
-  })
+})
+
+export const SignInValidation = z.object({
+  email : z.string().email(),
+  password : z.string().min(8,{message:'Password must be at least 8 characters'}),
+})
